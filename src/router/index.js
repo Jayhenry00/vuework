9a73d9165c2443770d65a803c7f5617fda3efa42
Jayhenry00 <18526237068@163.com>
2019-07-10 19:58:34 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import menu from '@/components/menu'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path:'/menu',
+      name:'menu',
+      component:menu
+    }
+  ]
+})
